08e227930d3940c10db0224305fe0d52bcecbba9
Karen Freitas <karenfcorrea@gmail.com>
2022-03-20 18:53:51 -0300

    implementando login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,32 @@
-import { UserService } from 'src/user/user.service';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import {JwtService} from '@nestjs/jwt'
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt'
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor (private userService: UserService, private jwtService: JwtService ){}
+    constructor(private userService: UserService, private jwtService: JwtService) { }
 
-  async login (email: string, password: string) {
-    const user = await this.userService.findUser(email)
-    if(!user){
-      throw new UnauthorizedException();
-    }
+    async login(email: string, password: string) {
+        const user = await this.userService.findUser(email)
 
-    const payload = { email, sub:user.id}
-    
-    return {
-      token: this.jwtService.sign(payload)
+        console.log(user);
+
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+        console.log(password, user.password)
+
+        const match = await bcrypt.compare(password, user.password)
+
+        if(match) {
+            const payload = { email, sub: user.id }
+
+            return {
+                token: this.jwtService.sign(payload)
+            }
+        } else {
+            throw new UnauthorizedException();
+        }
     }
-  }
 }
