07315b9ae86325d5eab7e21bd1ae901941b87c00
Karen Freitas <karenfcorrea@gmail.com>
2022-03-20 17:31:38 -0300

    bcrypt senhas

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import { Repository } from 'typeorm';
 import { User } from 'src/user/user.entity';
 import { Injectable } from '@nestjs/common';
 import UserDTO from './types/user.dto';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class UserService {
@@ -10,9 +11,16 @@ export class UserService {
     @InjectRepository(User) private userRepository: Repository<User>
 
   ){}
-  async createUser(user:UserDTO) {
-    const userEntity = this.userRepository.create(user)
-     return this.userRepository.save(userEntity)
 
+  async createUser(user:UserDTO) {
+    const encryptedPassword = await this.encryptPassword(user.password)
+    const userEntity = this.userRepository.create({...user, password: encryptedPassword})
+     return this.userRepository.save(userEntity)
   }
+
+  private encryptPassword(password:string) {
+    return bcrypt.hash(password, 10)
+  }
+
+
 }
